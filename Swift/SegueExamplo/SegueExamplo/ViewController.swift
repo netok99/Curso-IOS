0ed238bcf93274c0f0beae69b5229667b74ca68c
Gildenio Neto <neto@Mac-mini-de-Gildenio.local>
2016-02-20 10:13:02 -0200

    Aula 31 - Criando múltiplas View Controllers e usando Segues.

---
+++ Swift/SegueExamplo/SegueExamplo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegueExamplo
+//
+//  Created by Gildenio Neto on 20/02/16.
+//  Copyright © 2016 Gildenio Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
