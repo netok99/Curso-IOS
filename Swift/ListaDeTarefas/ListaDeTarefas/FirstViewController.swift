55be1aa921fbfcf55f093480501074d12fdc5c5e
Gildenio Neto <neto@Mac-mini-de-Gildenio.local>
2016-02-20 11:53:46 -0200

    Aula 34: App Lista de Tarefas

---
+++ Swift/ListaDeTarefas/ListaDeTarefas/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Gildenio Neto on 20/02/16.
+//  Copyright © 2016 Gildenio Neto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift/ListaDeTarefas/ListaDeTarefas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ListaDeTarefas
+//
+//  Created by Gildenio Neto on 20/02/16.
+//  Copyright © 2016 Gildenio Neto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
