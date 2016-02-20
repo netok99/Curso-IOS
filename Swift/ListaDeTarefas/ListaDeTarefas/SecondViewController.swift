99a89f9ce7e45762548ef70a5606b368edff94a0
Gildenio Neto <neto@Mac-mini-de-Gildenio.local>
2016-02-20 11:53:58 -0200

    Aula 34: App Lista de Tarefas

--- Swift/ListaDeTarefas/ListaDeTarefas/SecondViewController.swift
+++ Swift/ListaDeTarefas/ListaDeTarefas/SecondViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
+var toDoList = [String]()
+
 class SecondViewController: UIViewController {
 
+    @IBOutlet var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(item.text!)
+        item.text = "";
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,15 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldreturn(textField: UITextField!) -> Bool {
+        item.resignFirstResponder()
+        return true
+    }
 
 }
 
